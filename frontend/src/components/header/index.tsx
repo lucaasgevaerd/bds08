8dda0159a70b0ea741bb072c237a235375a886df
Lucas Gevaerd <lucaasgevaerd@gmail.com>
2022-08-23 23:39:09 -0300

    Project configured and header component created

---
+++ frontend/src/components/header/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+function Header() {
+    return (
+        <>
+            <header className="header-container">
+                <h1 className="header-title">BDS Vendas</h1>
+            </header>
+        </>
+    )
+}
+
+export default Header
